af169b56211a0dc82f00e78acec92afb99b85093
Eder Souza <edernorival@gmail.com>
2020-12-05 14:59:16 -0300

    implementation of services

--- src/main/java/com/coursewebservices/webservices/repositories/UserRepository.java
+++ src/main/java/com/coursewebservices/webservices/repositories/UserRepository.java
@@ -3,5 +3,5 @@ package com.coursewebservices.webservices.repositories;
 import com.coursewebservices.webservices.entities.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface UserRepository extends JpaRepository<User,Long> {
+public interface UserRepository extends JpaRepository<User, Long> {
 }
