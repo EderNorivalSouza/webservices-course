bc653d451ffbdd192b74b0d319b637ea86ac35fd
Eder Souza <edernorival@gmail.com>
2020-12-06 20:13:09 -0300

    exception handler findById

--- src/main/java/com/coursewebservices/webservices/services/UserService.java
+++ src/main/java/com/coursewebservices/webservices/services/UserService.java
@@ -2,6 +2,7 @@ package com.coursewebservices.webservices.services;
 
 import com.coursewebservices.webservices.entities.User;
 import com.coursewebservices.webservices.repositories.UserRepository;
+import com.coursewebservices.webservices.services.exceptions.ResourceNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -20,7 +21,7 @@ public class UserService {
 
     public User findById(Long id) {
         Optional<User> user = userRepository.findById(id);
-        return user.get();
+        return user.orElseThrow(()->new ResourceNotFoundException(id));
     }
 
     public User insert(User user) {
